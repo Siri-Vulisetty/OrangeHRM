7f66aeaeeb6362eeb5f34cf98326ab8a62114687
Siri-Vulisetty <sirivulisetty@gmail.com>
2026-05-30 20:49:52 +0530

    employeeListTest

--- src/main/java/tests/loginTest.java
+++ src/main/java/tests/loginTest.java
@@ -5,25 +5,27 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.loginPage;
+import utils.ConfigReader;
 import utils.basePage;
 
 public class loginTest extends basePage {
 
     loginPage login;
+   ConfigReader config= new ConfigReader();
 
 
 
     @BeforeMethod
     public void setUp() {
         openBrowser();
-        navigateTo("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        navigateTo(ConfigReader.read("baseurl"));
     }
 
     @Test
     public void validLoginTest() throws InterruptedException {
         login = new loginPage(driver);
         Thread.sleep(3000);
-        login.login("Admin", "admin123");
+        login.login(ConfigReader.read("username"), ConfigReader.read("password"));
         Thread.sleep(5000);
         Assert.assertTrue(login.isDashboardDisplayed(), "Dashboard is not displayed after login");
     }
---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,11 @@
+package utils;
+
+
+
+public class Constants {
+
+    public static final String CONFIG_FILE_PATH = System.getProperty("user.dir") + "/src/main/resources/config.properties";
+
+
+}
+
